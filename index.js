676cf50dfd8ad507b9044333fa8d675fe1783cf0
Özgür, Dr. <ozgur@phy-x.net>
2026-01-07 14:34:08 +0300

    swap: npm name changed

--- index.js
+++ index.js
@@ -13,18 +13,20 @@ window.randomUUID = () => (
             .concat(u64[0].toString(16).substring(4)).padEnd(36, 0)
     );
 
-let t0, tLen, dt, max = 1e6;
+function benchmark() {
+    let t0, tLen, dt, max = 1e6;
 
-tLen = max;
-t0 = performance.now();
-while (tLen--) crypto.randomUUID();
-dt = performance.now() - t0;
-console.log("crypto.randomUUID:", dt, crypto.randomUUID());
-// crypto.randomUUID: 1138.6050000190735 d47cb6aa-e781-452a-ba2b-03663bd0b67c
+    tLen = max;
+    t0 = performance.now();
+    while (tLen--) crypto.randomUUID();
+    dt = performance.now() - t0;
+    console.log("crypto.randomUUID:", dt, crypto.randomUUID());
+    // crypto.randomUUID: 1138.6050000190735 d47cb6aa-e781-452a-ba2b-03663bd0b67c
 
-tLen = max;
-t0 = performance.now();
-while (tLen--) window.randomUUID();
-dt = performance.now() - t0;
-console.log("window.randomUUID:", dt, window.randomUUID());
-// window.randomUUID: 426.0099997520447 f1235820-f090-3fce-a0d6-af6e26c6a0d6
+    tLen = max;
+    t0 = performance.now();
+    while (tLen--) window.randomUUID();
+    dt = performance.now() - t0;
+    console.log("window.randomUUID:", dt, window.randomUUID());
+    // window.randomUUID: 426.0099997520447 f1235820-f090-3fce-a0d6-af6e26c6a0d6
+}
